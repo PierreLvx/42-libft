8784503b3d8bee912c01c19cd2f0900cafcc1b5e
Pierre Lavaux <plavaux@student.42.fr>
2014-11-03 19:53:18 +0100

    fix putendl

--- ft_putendl.c
+++ ft_putendl.c
@@ -18,6 +18,6 @@
 
 void	ft_putendl(char const *s)
 {
-	ft_putstr(*s);
+	ft_putstr(s);
 	ft_putchar('\n');
 }
--- ft_putendl_fd.c
+++ ft_putendl_fd.c
@@ -16,7 +16,7 @@
 ** Prints the string s to the file descriptor fd, followed by a '\n'.
 */
 
-void	ft_putendl_fd(char const *s)
+void	ft_putendl_fd(char const *s, int fd)
 {
 	ft_putstr_fd(s, fd);
 	ft_putchar_fd('\n', fd);
