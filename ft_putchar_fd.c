f9d223813978cffb79520bd592a695d5d2480f23
Pierre Lavaux <plavaux@student.42.fr>
2014-11-03 21:12:58 +0100

    comment putchar + cleanup libft.h

--- ft_putchar_fd.c
+++ ft_putchar_fd.c
@@ -12,6 +12,10 @@
 
 #include <unistd.h>
 
+/*
+** Prints the character c on the specified filedescriptor.
+*/
+
 void	ft_putchar_fd(char c, int fd)
 {
 	write(fd, &c, 1);
--- libft.h
+++ libft.h
@@ -6,28 +6,30 @@
 /*   By: plavaux <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/11/03 11:40:29 by plavaux           #+#    #+#             */
-/*   Updated: 2014/11/03 18:44:47 by plavaux          ###   ########.fr       */
+/*   Updated: 2014/11/03 20:54:59 by plavaux          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
 # define LIBFT_H
 
-void	ft_putstr(char const *s);
-void	ft_putstr_fd(char const *s, int fd);
-void	ft_putchar(char c);
-void	ft_putchar_fd(char c, int fd);
-void	ft_putnbr(int n);
-void	ft_putnbr_fd(int n, int fd);
 int		ft_atoi(const char *str);
+int		ft_isalnum(int c);
 int		ft_isalpha(int c);
 int		ft_isascii(int c);
+int		ft_isdigit(int c);
 int		ft_islower(int c);
 int		ft_isprint(int c);
 int		ft_isupper(int c);
+void	ft_putchar(char c);
+void	ft_putchar_fd(char c, int fd);
+void	ft_putendl(char const *s);
+void	ft_putendl_fd(char const *s, int fd);
+void	ft_putnbr(int n);
+void	ft_putnbr_fd(int n, int fd);
+void	ft_putstr(char const *s);
+void	ft_putstr_fd(char const *s, int fd);
+//size_t	ft_strlen(const char *s);
 int		ft_tolower(int c);
 int		ft_toupper(int c);
-int		ft_isalnum(int c);
-int		ft_isdigit(int c);
-//size_t	ft_strlen(const char *s);
 #endif
