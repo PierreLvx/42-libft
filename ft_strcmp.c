f094dd65b421b37d5eb88db2013c2a783c3b5753
Pierre LAVAUX <plavaux@student.42.fr>
2014-11-04 15:56:15 +0100

    strcmp

---
+++ ft_strcmp.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strcmp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: plavaux <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2014/11/04 15:34:31 by plavaux           #+#    #+#             */
+/*   Updated: 2014/11/04 15:45:37 by plavaux          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*
+** Compares two null-terminated byte strings.
+** The comparison is done lexicographically.
+*/
+
+int	ft_strcmp(const char *s1, const char *s2)
+{
+	int	i;
+
+	i = 0;
+	while (s1[i] != '\0' && s2[i] != '\0' && s1[i] == s2[i])
+		i++;
+	return (s1[i] - s2[i]);
+}
