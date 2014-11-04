471715176a3141ea04478290ee4e11bc85a30b73
Pierre Lavaux <pierre.lvx@gmail.com>
2014-11-04 19:36:49 +0100

    strequ

---
+++ ft_strequ.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strequ.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: plavaux <plavaux@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2014/11/04 19:32:26 by plavaux           #+#    #+#             */
+/*   Updated: 2014/11/04 19:32:38 by plavaux          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+/*
+** Compare s1 and s2. If both strings are equal, returns 0, else returns 1.
+** The comparison is done lexicographically.
+*/
+
+int	ft_strequ(char const *s1, char const *s2)
+{
+	if (s1 && s2)
+		if (ft_strcmp(s1, s2) == 0)
+			return (1);
+	return (0);
+}
