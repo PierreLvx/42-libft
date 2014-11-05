0a5f00c2a27c0066dde3eda1a43d4f28d2639799
Pierre LAVAUX <plavaux@student.42.fr>
2014-11-05 11:40:50 +0100

    striter + striteri

---
+++ ft_striter.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_striter.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: plavaux <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2014/11/05 10:55:46 by plavaux           #+#    #+#             */
+/*   Updated: 2014/11/05 11:00:15 by plavaux          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+/*
+** Applies the function f to each character of the parameter string.
+** Each character is passed by address to the function.
+*/
+
+void	ft_striter(char *s, void (*f)(char *))
+{
+	while (*s)
+		f(s++);
+}
