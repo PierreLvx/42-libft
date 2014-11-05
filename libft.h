3fcef7507c42d756faa0aa435ec89cb3770a3d18
Pierre LAVAUX <plavaux@student.42.fr>
2014-11-05 15:15:15 +0100

    mo protos

--- libft.h
+++ libft.h
@@ -17,6 +17,7 @@
 # include <unistd.h>
 
 int		ft_atoi(const char *str);
+void	bzero(void *s, size_t n);
 int		ft_isalnum(int c);
 int		ft_isalpha(int c);
 int		ft_isascii(int c);
@@ -27,6 +28,7 @@ int		ft_isupper(int c);
 void	*ft_memchr(const void *s, int c, size_t n);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
 void	ft_memdel(void **ap);
+void	*ft_memset(void *b, int c, size_t len);
 void	ft_putchar(char c);
 void	ft_putchar_fd(char c, int fd);
 void	ft_putendl(char const *s);
@@ -36,11 +38,17 @@ void	ft_putnbr_fd(int n, int fd);
 void	ft_putstr(char const *s);
 void	ft_putstr_fd(char const *s, int fd);
 char	*ft_strcat(char *s1, char *s2);
+void	ft_strclr(char *s);
 int		ft_strcmp(const char *s1, const char *s2);
-int		ft_strncmp(const char *s1, const char *s2, size_t n);
 char	*ft_strcpy(char *dst, const char *src);
 void	ft_strdel(char **as);
+int		ft_strequ(char const *s1, char const *s2);
+void	ft_striter(char *s, void (*f)(char *));
+void	ft_striteri(char *s, void (*f)(unsigned int, char *));
 size_t	ft_strlen(const char *s);
+int		ft_strncmp(const char *s1, const char *s2, size_t n);
+int		ft_strnequ(char const *s1, char const *s2, size_t n);
+char	*ft_strstr(const char *s1, const char *s2);
 int		ft_tolower(int c);
 int		ft_toupper(int c);
 #endif
