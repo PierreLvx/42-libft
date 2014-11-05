1502d451bdcf0d8748e0b3eedc3d136d1c7e0842
Pierre LAVAUX <plavaux@student.42.fr>
2014-11-05 13:36:06 +0100

    memset

---
+++ ft_memset.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memset.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: plavaux <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2014/11/05 13:18:37 by plavaux           #+#    #+#             */
+/*   Updated: 2014/11/05 13:35:19 by plavaux          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+/*
+** Writes len bytes of value c (converted to an unsigned char) to the string b.
+** The function returns its first argument.
+*/
+
+void *ft_memset(void *b, int c, size_t len)
+{
+	size_t bytes;
+
+	bytes = 0;
+	while (bytes < len)
+	{
+		((char *)b)[bytes] = (unsigned char)c;
+		bytes++;
+	}
+	return (b);
+}
