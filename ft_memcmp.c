a5fb01181bedee7985efe795ee5095d4d392661f
Pierre LAVAUX <plavaux@student.42.fr>
2014-11-05 14:55:39 +0100

    memcmp

---
+++ ft_memcmp.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcmp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: plavaux <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2014/11/05 14:47:32 by plavaux           #+#    #+#             */
+/*   Updated: 2014/11/05 14:54:36 by plavaux          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+/*
+** Compares byte string s1 against byte string s2.
+** Both strings are assumed to be n bytes long.
+*/
+
+int	ft_memcmp(const void *s1, const void *s2, size_t n)
+{
+	size_t	i;
+
+	i = 0;
+	while (i < n)
+	{
+		if (((unsigned char *)s1)[i] != ((unsigned char *)s2)[i])
+			return (((unsigned char *)s1)[i] - ((unsigned char *)s2)[i]);
+		i++;
+	}
+	return (0);
+}
