ad820ec2344bcf5a75a4e8bc36037e223a638c3e
Pierre LAVAUX <plavaux@student.42.fr>
2014-11-07 17:46:02 +0100

    shorther putstr

--- ft_putstr.c
+++ ft_putstr.c
@@ -6,25 +6,19 @@
 /*   By: plavaux <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/11/03 15:11:31 by plavaux           #+#    #+#             */
-/*   Updated: 2014/11/05 15:50:34 by plavaux          ###   ########.fr       */
+/*   Updated: 2014/11/07 17:44:36 by plavaux          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <unistd.h>
 #include "libft.h"
 
 /*
-** Prints the string s on standard output
+** Print the string s on standard output.
 */
 
 void	ft_putstr(char const *s)
 {
-	int	i;
-
-	i = 0;
-	if (s != '\0')
-		while (s[i])
-		{
-			ft_putchar(s[i]);
-			i++;
-		}
+	if (s != NULL)
+		write(1, s, ft_strlen(s));
 }
