2a484f474b60ec23e5e4f190db78b13ddcf75214
Pierre LAVAUX <plavaux@student.42.fr>
2014-11-07 19:38:17 +0100

    Missing protos

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: plavaux <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/11/03 11:40:29 by plavaux           #+#    #+#             */
-/*   Updated: 2014/11/07 13:50:03 by plavaux          ###   ########.fr       */
+/*   Updated: 2014/11/07 19:34:21 by plavaux          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,18 +41,29 @@ void	ft_putnbr_fd(int n, int fd);
 void	ft_putstr(char const *s);
 void	ft_putstr_fd(char const *s, int fd);
 char	*ft_strcat(char *s1, const char *s2);
+char	*ft_strchr(const char *s, int c);
 void	ft_strclr(char *s);
 int		ft_strcmp(const char *s1, const char *s2);
 char	*ft_strcpy(char *dst, const char *src);
 void	ft_strdel(char **as);
+char	*ft_strdup(const char *s1);
 int		ft_strequ(char const *s1, char const *s2);
 void	ft_striter(char *s, void (*f)(char *));
 void	ft_striteri(char *s, void (*f)(unsigned int, char *));
+void	ft_strjoin(char const *s1, char const *s2);
+size_t	ft_strlcat(char *dst, const char *src, size_t size);
 size_t	ft_strlen(const char *s);
+char	*ft_strmap(char const *s, char (*f)(char));
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
+char	*ft_strncat(char *s1, const char *s2, size_t n);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
+char	*ft_strncpy(char *s1, const char *s2, size_t n);
 int		ft_strnequ(char const *s1, char const *s2, size_t n);
+char	*ft_strnew(size_t size);
+char	*ft_strnstr(const char *s1, const char *s2, size_t n);
+char	*ft_strrchr(const char *s, int c);
 char	*ft_strstr(const char *s1, const char *s2);
+char	*ft_strsub(char const *s, unsigned int start, size_t len);
 int		ft_tolower(int c);
 int		ft_toupper(int c);
-char	*ft_strmap(char const *s, char (*f)(char));
 #endif
