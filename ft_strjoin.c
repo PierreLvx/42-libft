5aa8740e60878dc69909220bc16214503d5cfa69
Pierre LAVAUX <plavaux@student.42.fr>
2014-11-07 16:42:42 +0100

    more strings

---
+++ ft_strjoin.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: plavaux <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2014/11/07 13:39:12 by plavaux           #+#    #+#             */
+/*   Updated: 2014/11/07 16:39:08 by plavaux          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+#include "libft.h"
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	size_t	len;
+	char	*out;
+
+	out = NULL;
+	if (s1 && s2)
+	{
+		len = ft_strlen(s1) + ft_strlen(s2);
+		out = (char *)malloc(sizeof(char) * (len + 1));
+		if (out)
+		{
+			ft_strcpy(out, s1);
+			ft_strcat(out, s2);
+		}
+	}
+	return (out);
+}
