8e15b78883f23c0dac80b05fb83194c1cf7fa4c9
Pierre LAVAUX <plavaux@student.42.fr>
2014-11-14 10:42:03 +0100

    strsplit

---
+++ ft_strsplit.c
@@ -0,0 +1,58 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strsplit.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: plavaux <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2014/11/14 10:28:04 by plavaux           #+#    #+#             */
+/*   Updated: 2014/11/14 10:28:48 by plavaux          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <stdlib.h>
+
+static int	count_words(char *s, char c)
+{
+	int		words;
+
+	while (*s && *s == c)
+		++s;
+	if (*s)
+		words = 1;
+	else
+		words = 0;
+	while (*s)
+	{
+		if (*s == c && s[1] && s[1] != c)
+			++words;
+		++s;
+	}
+	return (words);
+}
+
+char		**ft_strsplit(char const *s, char c)
+{
+	int		words;
+	char	*start;
+	char	**result;
+
+	words = count_words((char *)s, c);
+	result = (char **)malloc(sizeof(char *) * (count_words((char *)s, c) + 1));
+	start = (char *)s;
+	while (*s)
+	{
+		if (*s == c)
+		{
+			if (start != s)
+				*(result++) = ft_strsub(start, 0, s - start);
+			start = (char *)s + 1;
+		}
+		++s;
+	}
+	if (start != s)
+		*(result++) = ft_strsub(start, 0, s - start);
+	*result = NULL;
+	return (result - words);
+}
