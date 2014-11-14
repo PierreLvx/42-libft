fbd78e6fb54bffa14511fbf10bf27b00452c445b
Pierre LAVAUX <plavaux@student.42.fr>
2014-11-14 18:17:46 +0100

    Augmented assignment + sub function

--- ft_isspace.c
+++ ft_isspace.c
@@ -6,20 +6,15 @@
 /*   By: plavaux <plavaux@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/11/07 20:04:22 by plavaux           #+#    #+#             */
-/*   Updated: 2014/11/14 11:54:34 by plavaux          ###   ########.fr       */
+/*   Updated: 2014/11/14 18:15:39 by plavaux          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
 int		ft_isspace(int c)
 {
-	if (c == ' '
-		|| c == '\t'
-		|| c == '\n'
-		|| c == '\r'
-		|| c == '\v'
-		|| c == '\f')
-	{
+	if (ft_isblank(c) || c == '\r' || c == '\v' || c == '\f')
 		return (1);
-	}
 	return (0);
 }
