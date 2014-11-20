4c9d7fdc091f0190b084cecd34b4c212057c2179
Pierre Lavaux <PierreLvx@users.noreply.github.com>
2014-11-20 10:46:17 +0100

    Fix strcmp

--- ft_strcmp.c
+++ ft_strcmp.c
@@ -6,7 +6,7 @@
 /*   By: plavaux <plavaux@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/11/04 15:34:31 by plavaux           #+#    #+#             */
-/*   Updated: 2014/11/14 11:54:30 by plavaux          ###   ########.fr       */
+/*   Updated: 2014/11/20 10:46:30 by plavaux          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,5 +22,5 @@ int	ft_strcmp(const char *s1, const char *s2)
 	i = 0;
 	while (s1[i] != '\0' && s2[i] != '\0' && s1[i] == s2[i])
 		i++;
-	return (s1[i] - s2[i]);
+	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 }
