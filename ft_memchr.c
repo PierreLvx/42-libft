d636ef830cb2d6d381239f1fdade8b9220de2d07
Pierre LAVAUX <plavaux@student.42.fr>
2014-12-23 18:32:15 +0100

    fix memchr

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,7 +6,7 @@
 /*   By: plavaux <plavaux@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/11/04 16:38:00 by plavaux           #+#    #+#             */
-/*   Updated: 2014/11/14 11:54:33 by plavaux          ###   ########.fr       */
+/*   Updated: 2014/12/23 18:26:00 by plavaux          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,16 +16,19 @@
 ** Locates the 1st occurrence of c (converted to an unsigned char) in string s.
 */
 
-void	*ft_memchr(const void *s, int c, size_t n)
+void				*ft_memchr(const void *s, int c, size_t n)
 {
-	size_t	i;
+	size_t			i;
+	unsigned char	*cpy;
 
 	i = 0;
-	while (i < n)
-	{
-		if (((unsigned char *)s)[i] == (unsigned char)c)
-			return (&((unsigned char *)s)[i]);
-		i++;
-	}
+	cpy = (unsigned char *)s;
+	if (s && n > 0)
+		while (i < n)
+		{
+			if (cpy[i] == (unsigned char)c)
+				return ((void*)&cpy[i]);
+			i++;
+		}
 	return (NULL);
 }
