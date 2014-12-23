4455fd3afe9b31f48dfc75398ab454cf0ef61817
Pierre LAVAUX <plavaux@student.42.fr>
2014-12-23 18:31:22 +0100

    fix putnbr_fd

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -14,12 +14,14 @@
 
 void	ft_putnbr_fd(int n, int fd)
 {
-	if (n < 0)
+	if (n == -2147483648)
+		ft_putstr_fd("-2147483648", fd);
+	if (n < 0 && n > -2147483648)
 	{
 		ft_putchar_fd('-', fd);
 		n *= -1;
 	}
-	if (n < 10)
+	if (n < 10 && n > -2147483648)
 		ft_putchar_fd(n + 48, fd);
 	else if (n >= 10)
 	{
