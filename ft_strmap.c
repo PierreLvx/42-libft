09a89daed1f0df31c85180649f2c1993bf1af2c9
Pierre Lavaux <pierre.lvx@gmail.com>
2014-12-25 18:36:31 +0100

    more efficient strmap

--- ft_strmap.c
+++ ft_strmap.c
@@ -6,7 +6,7 @@
 /*   By: plavaux <plavaux@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/11/07 13:40:17 by plavaux           #+#    #+#             */
-/*   Updated: 2014/11/14 11:54:27 by plavaux          ###   ########.fr       */
+/*   Updated: 2014/12/25 18:36:05 by plavaux          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,25 +20,17 @@
 
 char	*ft_strmap(char const *s, char (*f)(char))
 {
-	size_t	len;
 	size_t	i;
 	char	*fresh;
 
-	fresh = NULL;
-	if (s && f)
+	if (!s || !f)
+		return (NULL);
+	fresh = ft_strnew(ft_strlen(s));
+	i = 0;
+	while (s[i] != '\0')
 	{
-		len = ft_strlen(s);
-		i = 0;
-		fresh = (char *)malloc(sizeof(char) * (len + 1));
-		if (fresh)
-		{
-			while (i < len)
-			{
-				fresh[i] = f(s[i]);
-				i++;
-			}
-			fresh[i] = '\0';
-		}
+		fresh[i] = (*f)(s[i]);
+		i++;
 	}
 	return (fresh);
 }
