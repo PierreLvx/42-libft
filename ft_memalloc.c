ad03c998530a830edcb18dffb066c08b7a72ccad
Pierre LAVAUX <plavaux@e3r5p11.42.fr>
2015-01-12 22:46:01 +0100

    removed useless comment ; changed counter to ptr

--- ft_memalloc.c
+++ ft_memalloc.c
@@ -6,17 +6,13 @@
 /*   By: plavaux <plavaux@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/11/06 18:46:52 by plavaux           #+#    #+#             */
-/*   Updated: 2014/11/08 14:26:58 by plavaux          ###   ########.fr       */
+/*   Updated: 2015/01/12 22:43:55 by plavaux          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <string.h>
 #include <stdlib.h>
 #include "libft.h"
-
-/*
-** Casting to void * is redundant
-*/
 
 void	*ft_memalloc(size_t size)
 {
